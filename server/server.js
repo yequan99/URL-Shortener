06be11b73746af1bded01a72af2e3a0586ec2aab
yequan99 <ytan213@e.ntu.edu.sg>
2023-08-04 21:04:22 +0800

    Initialise mongodb connection

--- server/server.js
+++ server/server.js
@@ -1,6 +1,22 @@
 const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
 const app = express()
-const port = 8080
+const port = process.env.PORT || 5050
+
+app.use(cors())
+app.use(express.json())
+
+const uri = process.env.ATLAS_URI
+mongoose.connect(uri, {
+    useNewUrlParser: true
+})
+.then(() => console.log('MongoDB connection established.'))
+.catch((error) => console.error("MongoDB connection failed:", error.message))
+
 
 app.get('/', (req,res) => res.send("Hello World"))
 
