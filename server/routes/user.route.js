186da7a1508a1f1c2317b04e5f4b398781c44c24
yequan99 <ytan213@e.ntu.edu.sg>
2023-08-05 11:38:19 +0800

    Apply salt and hashing for password

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,26 +1,37 @@
 const router = require('express').Router()
+const bcrypt = require("bcryptjs")
 let User = require('../models/user.model')
 
-router.route('/').get((req, res) => {
+router.get('/', (req, res) => {
     User.find()
         .then(users => res.json(users))
         .catch(err => res.status(400).json('Error: ' + err))
 })
 
-router.route('/add').post((req, res) => {
-    const username = req.body.username
-    const salt = req.body.salt
-    const hash = req.body.hash
+router.post('/add', async (req, res) => {
+    // Check if username exists
+    const usernameExist = await User.findOne({ username: req.body.username})
+
+    if (usernameExist) {
+        res.status(400).send("Username already exists")
+        return
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
 
     const newUser = new User({
-        username,
-        salt,
-        hash,
+        username: req.body.username,
+        salt: salt,
+        hash: hashedPassword,
     })
 
-    newUser.save()
-        .then(() => res.json("User added!"))
-        .catch(err => res.status(400).json('Error: ' + err))
+    try {
+        const saveUser = await newUser.save()
+        res.status(200).json("User Created!")
+    } catch (err) {
+        res.status(400).json('[User Creation] Error: ', + err)
+    }
 })
 
 module.exports = router
