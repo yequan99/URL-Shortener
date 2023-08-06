9155865c146d0c16f61579e2a8b8a0632ece12f2
yequan99 <ytan213@e.ntu.edu.sg>
2023-08-06 16:38:48 +0800

    Button to redirect to shorturl

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,5 +1,6 @@
 import moment from 'moment'
 import { RxCross2 } from 'react-icons/rx'
+import { FiExternalLink } from 'react-icons/fi'
 import DeleteURL from '../api/DeleteURLDataAPI'
 import { DeleteUrlMsg } from '../type/struct'
 
@@ -20,13 +21,21 @@ export default function Card({itemID, longurl, shorturl, date}: {itemID: number,
 
     return (
         <div className="group border-2 border-slate-300">
-            <div className="m-4">
-                <div className="flex flex-row justify-between">
+            <div className="m-4 flex flex-row">
+                <div className="w-[80%]">
                     <h1 className="font-bold">{shorturl}</h1>
-                    <div className="invisible cursor-pointer group-hover:visible" onClick={() => deleteURL(itemID)}><RxCross2 /></div>
+                    <h1 className="text-xs py-2 text-green-600">{longurl}</h1>
+                    <h1 className="italic text-xs text-slate-500">{getDateDiff(date)}</h1>
                 </div>
-                <h1 className="text-xs py-2 text-green-600">{longurl}</h1>
-                <h1 className="italic text-xs text-slate-500">{getDateDiff(date)}</h1>
+                <div className="w-[20%] flex flex-col items-end justify-between">
+                    <div className="invisible cursor-pointer group-hover:visible" onClick={() => deleteURL(itemID)}><RxCross2 /></div>
+                    <div className="invisible cursor-pointer group-hover:visible">
+                        <a href={`https://${shorturl}`} target="_blank" rel="noopener noreferrer">
+                            <FiExternalLink />
+                        </a>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
