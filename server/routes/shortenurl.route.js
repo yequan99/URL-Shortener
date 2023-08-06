099c52791ae0b7dab338770551c98b39d7886752
yequan99 <ytan213@e.ntu.edu.sg>
2023-08-06 22:07:30 +0800

    Added mutex lock to prevent race condition

--- server/routes/shortenurl.route.js
+++ server/routes/shortenurl.route.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
-const shortid = require('shortid')
+const mutexify = require('mutexify')
+const lock = mutexify()
 
 const auth = require('../middleware/auth')
 let UserURLModel = require('../models/userURL.model')
@@ -17,43 +18,49 @@ router.get('/', async (req, res) => {
 })
 
 router.post('/shorten', async (req, res) => {
-    try {
-        const userID = req.header("x-user-id")
-        const longURL = req.body.longURL
-
-        // Validate URL
-        const validate = ValidateURL(longURL)
-        if (!validate){
-            console.log("[URL Shorten] Invalid URL parsed")
-            res.status(400).json({ "Error": "Invalid URL"})
-        } else {
-            // Check if url already exists in user DB
-            const existUrl = await UserURLModel.findOne({ userID: userID, longurl: longURL })
-            if (existUrl) {
-                // Return stored short url
-                const shortURL = existUrl.shorturl
-                res.status(200).json({ "shortURL": shortURL })
+    // Mutex lock to prevent race condition when assigning url code
+    lock(async function(release) {
+        try {
+            const userID = req.header("x-user-id")
+            const longURL = req.body.longURL
+    
+            // Validate URL
+            const validate = ValidateURL(longURL)
+            if (!validate){
+                console.log("[URL Shorten] Invalid URL parsed")
+                res.status(400).json({ "Error": "Invalid URL"})
             } else {
-                // Generate unique short url code per user
-                const searchOtherUser = await UserURLModel.find({ longurl: longURL })
-                const uniqueUrlCode = GenUniqueUrlCode(searchOtherUser)
-
-                const shortURL = process.env.BASE_URL + '/' + uniqueUrlCode
-                const newUserURL = new UserURLModel({
-                    userID: userID,
-                    longurl: longURL,
-                    shorturl: shortURL,
-                    urlcode: uniqueUrlCode
-                })
-
-                const saveUserURL = await newUserURL.save()
-                res.status(200).json({ "shortURL": shortURL })
+                // Check if url already exists in user DB
+                const existUrl = await UserURLModel.findOne({ userID: userID, longurl: longURL })
+                if (existUrl) {
+                    // Return stored short url
+                    const shortURL = existUrl.shorturl
+                    res.status(200).json({ "shortURL": shortURL })
+                } else {
+                    // Generate unique short url code per user
+                    const searchOtherUser = await UserURLModel.find({ longurl: longURL })
+                    const uniqueUrlCode = GenUniqueUrlCode(searchOtherUser)
+    
+                    const shortURL = process.env.BASE_URL + '/' + uniqueUrlCode
+                    const newUserURL = new UserURLModel({
+                        userID: userID,
+                        longurl: longURL,
+                        shorturl: shortURL,
+                        urlcode: uniqueUrlCode
+                    })
+    
+                    const saveUserURL = await newUserURL.save()
+                    res.status(200).json({ "shortURL": shortURL })
+                }
             }
+        } catch (err) {
+            console.error("Error: ", err)
+            res.status(500).json({ "Error": "Server Error" })
+        } finally {
+            release() //Release lock
         }
-    } catch (err) {
-        console.error("Error: ", err)
-        res.status(500).json({ "Error": "Server Error" })
-    }
+    })
+    
 })
 
 router.post('/delete', async (req, res) => {
